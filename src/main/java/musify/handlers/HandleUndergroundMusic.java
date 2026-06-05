a8bf633ac629cea2706116724a2096873fe0a921
Vortron-rd <119626603+Vortron-rd@users.noreply.github.com>
2026-06-05 16:51:10 -0500

    Fix overlapping music when using biome tags and cavern music.

--- src/main/java/musify/handlers/HandleUndergroundMusic.java
+++ src/main/java/musify/handlers/HandleUndergroundMusic.java
@@ -9,30 +9,32 @@ import java.util.Random;
 import static musify.config.BiomeMusicConfig.iundergroundOptions;
 import static musify.config.BiomeMusicConfig.lfadeOptions;
 import static musify.handlers.BiomeMusicEventHandler.activeMusic;
+import static musify.handlers.BiomeMusicEventHandler.activeTagMusic;
 
 public class HandleUndergroundMusic {
 
     private static final Random random = new Random();
     public static boolean isUndergroundMusicPlaying = false;
 
-
+    @SuppressWarnings("ConstantConditions")
     public static void handleUndergroundMusic() {
+        System.out.println("***********\nCalled handleUndergroundMusic\n**************");
         if (!isUndergroundMusicPlaying) {
+            String musicFile = getRandomSongForCavern();
+
+            /* Check for Biome Tag Music */
+            if (activeTagMusic != null && !activeTagMusic.isFading()) {
+                activeTagMusic.stopWithFadeOut(lfadeOptions.customMusicFadeOutTime);
+            }
+
             if (activeMusic != null && !activeMusic.isFading()) {
                 activeMusic.stopWithFadeOut(lfadeOptions.customMusicFadeOutTime);
-                String musicFile = getRandomSongForCavern();
-                if (!musicFile.equals("default_music") && musicFile != null) {
-                    isUndergroundMusicPlaying = true;
-                    activeMusic = new MusicPlayer(musicFile, false);
-                    activeMusic.playWithFadeIn(lfadeOptions.customMusicFadeInTime);
-                } else return;
-            } else if (activeMusic == null) {
-                String musicFile = getRandomSongForCavern();
-                if (!musicFile.equals("default_music") && musicFile != null) {
-                    isUndergroundMusicPlaying = true;
-                    activeMusic = new MusicPlayer(musicFile, false);
-                    activeMusic.playWithFadeIn(lfadeOptions.customMusicFadeInTime);
-                } else return;
+            }
+
+            if (!musicFile.equals("default_music") && musicFile != null) {
+                isUndergroundMusicPlaying = true;
+                activeMusic = new MusicPlayer(musicFile, false);
+                activeMusic.playWithFadeIn(lfadeOptions.customMusicFadeInTime);
             }
         }
     }
